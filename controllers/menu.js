139e7cd7cc8639e5f8d476c7391958074d0273e8
adil tahiri <rajaadil1995@gmail.com>
2022-09-09 02:09:17 +0100

    add menu controllers

---
+++ controllers/menu.js
@@ -0,0 +1,9 @@
+const Menu = require("../models/Menu");
+
+module.exports = {
+  getMenu: async (req, res) => {},
+  getEditMenu: async (req, res) => {},
+  DeleteMenu: async (req, res) => {},
+  postSaveMenu: async (req, res) => {},
+  postNewMenu: async (req, res) => {},
+};
