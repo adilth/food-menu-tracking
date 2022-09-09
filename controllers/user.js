fdc7cc9cc25d25fe647766f2a83a165726f12ca5
adil tahiri <rajaadil1995@gmail.com>
2022-09-09 02:09:42 +0100

    add user controllers

---
+++ controllers/user.js
@@ -0,0 +1,9 @@
+const User = require("../models/User");
+
+module.exports = {
+  getLogin: async (req, res) => {},
+  getSignup: async (req, res) => {},
+  getLogout: async (req, res) => {},
+  postLogin: async (req, res) => {},
+  postSignup: async (req, res) => {},
+};
