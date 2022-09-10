a2bf405c03b318e0761a57cf0cc4f6f30b978bfd
adil tahiri <rajaadil1995@gmail.com>
2022-09-10 01:48:09 +0100

    add route method for home and search

--- controllers/home.js
+++ controllers/home.js
@@ -15,5 +15,28 @@ module.exports = {
       console.error(e);
     }
   },
-  getSearch: async (req, res) => {},
+  getSearch: async (req, res) => {
+    let ssh = req.session;
+    let q = req.body.search.searchInput;
+    let menuData = null;
+    let qry = { name: { $regex: "^" + q, $options: "i" } };
+    try {
+      if (q != null) {
+        let menuResult = await Menu.find(qry);
+        menuData = menuResult.data;
+      } else {
+        q = "search";
+        let menuResult = await Menu.find({});
+        menuData = menuResult.data;
+      }
+      res.render("home", {
+        title: "Menu Tracker",
+        data: menuData,
+        search: q,
+        loggedIn: ssh.loggedIn,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
