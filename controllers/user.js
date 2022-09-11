01f8681919f4d274a0212f5b64f6cbc676d62525
adil tahiri <rajaadil1995@gmail.com>
2022-09-11 02:22:26 +0100

    add login and register page method

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,17 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  getLogin: async (req, res) => {},
-  getSignup: async (req, res) => {},
-  getLogout: async (req, res) => {},
+  getLogin: async (req, res) => {
+    res.render("login", { title: "login", loggedIn: false, error: null });
+  },
+  getSignup: async (req, res) => {
+    res.render("register", { title: "register", loggedIn: false, error: null });
+  },
+  getLogout: async (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+  },
   postLogin: async (req, res) => {},
   postSignup: async (req, res) => {},
 };
