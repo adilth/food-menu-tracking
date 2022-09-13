8a3e4a2ddf58ade492533a41b8b9f98835c78eb2
adil tahiri <rajaadil1995@gmail.com>
2022-09-13 02:16:17 +0100

    add getmenu method

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,7 +1,9 @@
 const Menu = require("../models/Menu");
 
 module.exports = {
-  getMenu: async (req, res) => {},
+  getMenu: async (req, res) => {
+    res.render('menu',{title: 'Menu Page'})
+  },
   getEditMenu: async (req, res) => {},
   DeleteMenu: async (req, res) => {},
   postSaveMenu: async (req, res) => {},
