9dbf87b7bc418c85a4fc9b6a74580326bf5fa28b
adil tahiri <rajaadil1995@gmail.com>
2022-09-20 01:17:22 +0100

    add delete and add new menu

--- controllers/menu.js
+++ controllers/menu.js
@@ -34,7 +34,77 @@ module.exports = {
       });
     }
   },
-  DeleteMenu: async (req, res) => {},
-  postSaveMenu: async (req, res) => {},
-  postNewMenu: async (req, res) => {},
+  DeleteMenu: async (req, res) => {
+    let ssh = req.session;
+
+    if (!ssh.loggedIn) {
+      res.redirect("/login");
+    } else {
+      let menu = schemas.menu;
+      let menuId = req.params.id;
+      let qry = { _id: menuId };
+      await menu.deleteOne(qry);
+      res.redirect("/");
+    }
+  },
+  postSaveMenu: async (req, res) => {
+    let ssh = req.session;
+
+    if (!ssh.loggedIn) {
+      res.redirect("/login");
+    } else {
+      console.log(req.body);
+      let menuId = req.body.menuId;
+      let menuName = req.body.menuName;
+      let menuIcon = req.body.menuIcon;
+      let menuUrl = req.body.menuUrl;
+      let menu = schemas.menu;
+
+      let qry = { _id: menuId };
+
+      let saveData = {
+        $set: {
+          name: menuName,
+          icon: menuIcon,
+          menuUrl: menuUrl,
+        },
+      };
+
+      await menu.updateOne(qry, saveData);
+
+      res.redirect("/");
+    }
+  },
+  postNewMenu: async (req, res) => {
+    let ssh = req.session;
+
+    if (!ssh.loggedIn) {
+      res.redirect("/login");
+    } else {
+      console.log(req.body);
+      console.log("working menu");
+      let menuName = req.body.menuName;
+      let menuIcon = req.body.menuIcon;
+      let menuUrl = req.body.menuUrl;
+      //let userId = req.user.id
+      let menu = schemas.menu;
+
+      let qry = { name: menuName };
+
+      let searchResults = await menu.findOne(qry).then(async (userData) => {
+        if (!userData) {
+          // ok to add menu
+          let newMenu = new schemas.menu({
+            name: menuName,
+            icon: menuIcon,
+            menuUrl: menuUrl,
+          });
+
+          await newMenu.save();
+        }
+      });
+
+      res.redirect("/");
+    }
+  },
 };
